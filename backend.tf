b4541cff1b70f77e8fc8dfa126aeaf532448e490
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-04-16 12:47:10 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "nsh-usecase1"
-    key            = "env:/dev/terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"
+    bucket = "alb-project-bucket"  # Replace with your unique S3 bucket name
+    key    = "path/to/my/statefile/terraform.tfstate"  # Path inside the bucket to store the state
+    region = "us-east-1"  # Replace with your desired AWS region
+    encrypt = true  # Enable encryption of the state file
+    acl     = "private"  # Ensure the bucket is private
   }
 }
