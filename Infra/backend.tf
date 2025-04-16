de4d8bf5af3f74a28a0078c0e3bb4befef4fde42
Bala Subba Reddy <balasubba.reddy@hcltech.com>
2025-04-16 12:43:57 +0530

    done

--- Infra/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "alb-modules"  # Replace with your unique S3 bucket name
-    key    = "path/to/my/statefile/terraform.tfstate"  # Path inside the bucket to store the state
-    region = "us-east-1"  # Replace with your desired AWS region
-    encrypt = true  # Enable encryption of the state file
-    acl     = "private"  # Ensure the bucket is private
-  }
-}
